19205d23e5e4439518fc0f98264e5761f8c77055
PythonJay <get@doxed.tech>
2016-08-25 21:40:09 -0700

    Update MassRole.py

--- MassRole/MassRole.py
+++ MassRole/MassRole.py
@@ -13,8 +13,8 @@ class BetaMassAdd:
 		await self.bot.say("Adding everyone to role ` " + RankRole + " `, please wait.")
 		role = discord.utils.get(ctx.message.server.roles, name=RankRole)
 		for RankRole in ctx.message.server.members:
-			await self.bot.add_roles(RankRole, role)
+		await self.bot.add_roles(RankRole, role)
 		await self.bot.say("Finsihed adding roles.")
 
 def setup(bot):
-	bot.add_cog(BetaMassAss(bot))
+	bot.add_cog(BetaMassAdd(bot))
