1853bd7ae44c11c517a9ced77fc6a95e63b87745
PythonJay <get@doxed.tech>
2016-08-25 21:47:52 -0700

    Create Counter.py

---
+++ Counter/Counter.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import asyncio
+
+class JCounter:
+    """Displays how many channels and roles are in a server."""
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.command(pass_context=True, no_pm=True)
+    async def serverstats(self, ctx):
+        """Displays how many channels and roles are in a server."""
+        await self.bot.say("***Calculating***")
+        await self.bot.say("**Roles:** {}".format(len(ctx.message.server.roles)))
+        await self.bot.say("**Channels:** {}".format(len(ctx.message.server.channels)))
+
+def setup(bot):
+    bot.add_cog(JCounter(bot))
